0ed105c520f49b4282da910ea3ff309262d2dab2
Azaz ali <aizaz.0938@gmail.com>
2024-01-18 19:51:23 +0500

    add today deal section

--- app/components/dayDeal/DealBanner.jsx
+++ app/components/dayDeal/DealBanner.jsx
@@ -2,23 +2,11 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 const DealBanner = () => {
-  const counterDurationInHours = 3;
-  const [secRemaining, setSecRemaining] = useState(function () {
-    const storedValue =
-      localStorage.getItem("timer") || counterDurationInHours * 60 * 60;
-    return JSON.parse(storedValue);
-  });
+  const [secRemaining, setSecRemaining] = useState(10800);
   let mins = Math.floor(secRemaining / 60);
   mins = mins % 60;
   const seconds = Math.floor(secRemaining % 60);
   const hours = Math.floor(secRemaining / 60 / 60);
-
-  useEffect(() => {
-    localStorage.setItem("timer", JSON.stringify(secRemaining));
-  }, [secRemaining]);
-  useEffect(() => {
-    localStorage.setItem("timer", JSON.stringify(secRemaining));
-  }, [secRemaining]);
 
   useEffect(function () {
     const id = setInterval(function () {
@@ -27,9 +15,9 @@ const DealBanner = () => {
     return () => clearInterval(id);
   }, []);
   return (
-    <div className=" mx-auto flex min-h-32 max-w-[960px] items-center justify-between gap-6 rounded-xl bg-black px-6 text-white shadow-lg sm:min-h-40">
-      <div className="flex flex-[1.5] flex-col items-center gap-2  sm:flex-row">
-        <h3 className=" text-sm font-semibold uppercase sm:text-lg">
+    <div className=" mx-auto flex min-h-32 max-w-[960px] items-center justify-between gap-6 rounded-xl bg-black px-3 text-white shadow-lg sm:min-h-40 sm:px-6">
+      <div className="flex flex-[1.5] flex-col items-start gap-2  sm:flex-row  sm:items-center">
+        <h3 className=" whitespace-nowrap text-sm font-semibold uppercase sm:text-lg">
           Daily deals
         </h3>
         <p className=" whitespace-nowrap rounded-full bg-red-700 px-5 text-[9px] font-light sm:px-8">
@@ -38,10 +26,10 @@ const DealBanner = () => {
         </p>
       </div>
       <div className="flex flex-[1] flex-col items-center gap-3 sm:flex-[2] sm:gap-6 ">
-        <p className="  text-xs font-extralight capitalize text-gray-200 sm:-mb-2 sm:text-base">
+        <p className="  text-xs font-light capitalize text-gray-200 sm:-mb-2 sm:text-base">
           ending in
         </p>
-        <p className=" text-sm font-medium leading-3 sm:text-4xl">
+        <p className=" text-xl font-medium leading-3 sm:text-4xl">
           {secRemaining > 0 ? (
             ` ${hours < 10 && "0"}${hours} : ${mins < 10 ? "0" : ""}${mins} : ${
               seconds < 10 ? "0" : ""
@@ -52,7 +40,7 @@ const DealBanner = () => {
             </span>
           )}
         </p>
-        <p className="space-x-4 text-[9px] font-extralight sm:text-sm">
+        <p className="space-x-2 text-[9px] font-light sm:space-x-4 sm:text-sm">
           {" "}
           <span>hours</span>
           <span>minutes</span>
